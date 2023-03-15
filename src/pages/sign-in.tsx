cc7c3298c6e5579292114954d59608704d52f21d
radu-lefter <68811789+radu-lefter@users.noreply.github.com>
2023-03-15 18:37:51 +0000

    created reusable components for the form

--- src/pages/sign-in.tsx
+++ src/pages/sign-in.tsx
@@ -1,9 +1,20 @@
 import Page from '../components/Page';
+import Button from '../components/Button';
+import Field from '../components/Field';
+import Input from '../components/Input';
 
 const SignInPage: React.FC = () => {
   return (
     <Page title="Sign In">
-
+      <form>
+        <Field label="Email">
+          <Input type="email" />
+        </Field>
+        <Field label="Password">
+          <Input type="password" />
+        </Field>
+        <Button type="submit">Sign In</Button>
+      </form>
     </Page>
   );
 };
