557113e0ac01179c7c1da2b312224a1f4f2ec0dd
raduubraduu <lefter_radu@yahoo.com>
2023-03-24 19:04:47 +0000

    seting the error and loading states

--- src/pages/sign-in.tsx
+++ src/pages/sign-in.tsx
@@ -1,22 +1,29 @@
-import { FormEventHandler, useState } from 'react';
-import Page from '../components/Page';
-import Button from '../components/Button';
-import Field from '../components/Field';
-import Input from '../components/Input';
-import { fetchJson } from '../lib/api';
+import { FormEventHandler, useState } from "react";
+import Page from "../components/Page";
+import Button from "../components/Button";
+import Field from "../components/Field";
+import Input from "../components/Input";
+import { fetchJson } from "../lib/api";
 
 const SignInPage: React.FC = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [status, setStatus] = useState({ loading: false, error: false });
 
   const handleSubmit: FormEventHandler<HTMLFormElement> = async (event) => {
     event.preventDefault();
-    const response = await fetchJson('http://127.0.0.1:1337/api/auth/local', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ identifier: email, password }),
-    });
-    console.log('sign in:', response);
+    setStatus({ loading: true, error: false });
+    try {
+      const response = await fetchJson("http://127.0.0.1:1337/api/auth/local", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ identifier: email, password }),
+      });
+      setStatus({ loading: false, error: false });
+      console.log("sign in:", response);
+    } catch (err) {
+      setStatus({ loading: false, error: true });
+    }
   };
 
   return (
@@ -38,7 +45,12 @@ const SignInPage: React.FC = () => {
             onChange={(event) => setPassword(event.target.value)}
           />
         </Field>
-        <Button type="submit">Sign In</Button>
+        {status.error && <p className="text-red-700">Invalid credentials</p>}
+        {status.loading ? (
+          <p>Loading...</p>
+        ) : (
+          <Button type="submit">Sign In</Button>
+        )}
       </form>
     </Page>
   );
