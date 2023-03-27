5d12a7b7e9e7d0d1df49c4d65fbc29d7034267f1
raduubraduu <lefter_radu@yahoo.com>
2023-03-27 17:32:46 +0100

    installed react-query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,15 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "react-query";
 
-const  App: React.FC<AppProps> =({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
-}
+const queryClient = new QueryClient();
+
+const App: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
+};
 
 export default App;
