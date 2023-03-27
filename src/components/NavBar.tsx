d379716c42c5c4d4880cde1a41b4dbd6ba8a5446
raduubraduu <lefter_radu@yahoo.com>
2023-03-27 19:20:42 +0100

    created cart component

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,5 @@
-import Link from 'next/link';
-import { useSignOut, useUser } from '../hooks/user';
-
+import Link from "next/link";
+import { useSignOut, useUser } from "../hooks/user";
 
 const NavBar: React.FC = () => {
   const user = useUser();
@@ -11,27 +10,22 @@ const NavBar: React.FC = () => {
     <nav className="px-2 py-1 text-sm">
       <ul className="flex gap-2">
         <li className="text-lg font-extrabold">
-          <Link href="/">
-            Next Shop
-          </Link>
+          <Link href="/">Next Shop</Link>
         </li>
         <li role="separator" className="flex-1" />
         {user ? (
           <>
             <li>
-              {user.name}
+              <Link href="/cart">Cart</Link>
             </li>
+            <li>{user.name}</li>
             <li>
-              <button onClick={signOut}>
-                Sign Out
-              </button>
+              <button onClick={signOut}>Sign Out</button>
             </li>
           </>
         ) : (
           <li>
-            <Link href="/sign-in">
-              Sign In
-            </Link>
+            <Link href="/sign-in">Sign In</Link>
           </li>
         )}
       </ul>
