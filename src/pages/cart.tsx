bad2279acdf51415ef410071bbaaeda984785943
raduubraduu <lefter_radu@yahoo.com>
2023-03-28 19:13:24 +0100

    added functionality to display cart price

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -3,11 +3,32 @@ import { useQuery } from "react-query";
 import { fetchJson } from "../lib/api";
 import { CartItem } from "../lib/cart";
 
+interface Cart {
+  items: (CartItem & { total: number })[];
+  total: number;
+}
+
 interface CartTableProps {
   cartItems: CartItem[];
 }
 
+function formatCurrency(value: number): string {
+  return "$" + value.toFixed(2);
+}
+
+function buildCart(cartItems: CartItem[]): Cart {
+  let total = 0.0;
+  const items = [];
+  for (const cartItem of cartItems) {
+    const itemTotal = cartItem.product.price * cartItem.quantity;
+    total += itemTotal;
+    items.push({ ...cartItem, total: itemTotal });
+  }
+  return { items, total };
+}
+
 const CartTable: React.FC<CartTableProps> = ({ cartItems }) => {
+  const cart = buildCart(cartItems);
   return (
     <table>
       <thead>
@@ -15,17 +36,31 @@ const CartTable: React.FC<CartTableProps> = ({ cartItems }) => {
           <th className="px-4 py-2">Product</th>
           <th className="px-4 py-2">Price</th>
           <th className="px-4 py-2">Quantity</th>
+          <th className="px-4 py-2">Total</th>
         </tr>
       </thead>
       <tbody>
-        {cartItems.map((cartItem) => (
+        {cart.items.map((cartItem) => (
           <tr key={cartItem.id}>
             <td className="px-4 py-2">{cartItem.product.title}</td>
-            <td className="px-4 py-2 text-right">{cartItem.product.price}</td>
+            <td className="px-4 py-2 text-right">
+              {formatCurrency(cartItem.product.price)}
+            </td>
             <td className="px-4 py-2 text-right">{cartItem.quantity}</td>
+            <td className="px-4 py-2 text-right">
+              {formatCurrency(cartItem.total)}
+            </td>
           </tr>
         ))}
       </tbody>
+      <tfoot>
+        <tr>
+          <th className="px-4 py-2 text-left">Total</th>
+          <th></th>
+          <th></th>
+          <th className="px-4 py-2 text-right">{formatCurrency(cart.total)}</th>
+        </tr>
+      </tfoot>
     </table>
   );
 };
