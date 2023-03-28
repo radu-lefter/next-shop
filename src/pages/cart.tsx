d7b033ebb06b11dc46c0ba4fff03a067ac0e7a2a
raduubraduu <lefter_radu@yahoo.com>
2023-03-28 19:04:51 +0100

    displayed fetched dta into cart

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,17 +1,42 @@
-import Page from '../components/Page';
-import { useQuery } from 'react-query';
-import { fetchJson } from '../lib/api';
-import { CartItem } from '../lib/cart';
+import Page from "../components/Page";
+import { useQuery } from "react-query";
+import { fetchJson } from "../lib/api";
+import { CartItem } from "../lib/cart";
+
+interface CartTableProps {
+  cartItems: CartItem[];
+}
+
+const CartTable: React.FC<CartTableProps> = ({ cartItems }) => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th className="px-4 py-2">Product</th>
+          <th className="px-4 py-2">Price</th>
+          <th className="px-4 py-2">Quantity</th>
+        </tr>
+      </thead>
+      <tbody>
+        {cartItems.map((cartItem) => (
+          <tr key={cartItem.id}>
+            <td className="px-4 py-2">{cartItem.product.title}</td>
+            <td className="px-4 py-2 text-right">{cartItem.product.price}</td>
+            <td className="px-4 py-2 text-right">{cartItem.quantity}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
 
 const CartPage: React.FC = () => {
-  const query = useQuery<CartItem[]>('cartItems', () => fetchJson('/api/cart'));
+  const query = useQuery<CartItem[]>("cartItems", () => fetchJson("/api/cart"));
   const cartItems = query.data;
 
-  console.log('[CartPage] cartItems:', cartItems);
+  console.log("[CartPage] cartItems:", cartItems);
   return (
-    <Page title="Cart">
-
-    </Page>
+    <Page title="Cart">{cartItems && <CartTable cartItems={cartItems} />}</Page>
   );
 };
 
